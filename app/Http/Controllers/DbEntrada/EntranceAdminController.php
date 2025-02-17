5ce98919dab30759198f27dcd53ad05d73395318
Dann <robindannjf@gmail.com>
2025-02-17 14:07:05 -0500

    validación de fecha de finalizacion sea siempre mayor que la de inicio x2

--- app/Http/Controllers/DbEntrada/EntranceAdminController.php
+++ app/Http/Controllers/DbEntrada/EntranceAdminController.php
@@ -50,8 +50,6 @@ class EntranceAdminController extends Controller
            'name' => 'required',
            'start_date' => 'required|date',
            'end_date' => 'required|date|after_or_equal:start_date'
-           
-
         ]);
 
     
